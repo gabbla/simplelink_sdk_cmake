a3049a2db87f96fc12710a3c4e6c61e3f2f2dd58
Gabriele Zampieri <zampierigabriele.ist@gmail.com>
2020-02-26 23:33:15 +0100

    cc26x2: Fixed typo on variable

--- cc26x2.cmake
+++ cc26x2.cmake
@@ -10,13 +10,12 @@ message(STATUS "Target device: SimpleLink CC26x2")
 # -march=armv7e-m       Specifies the name of the target ARM architecture.
 # -mfloat-abi=hard      Specifies which floating-point ABI to use.
 # -mfpu=fpv4-sp-d16     Specify the FPU available in hw.
-set(OBJ_GEN_FLAGS -mcpu=cortex-m4 -march=armv7e-m -mfloat-abi=hard
-    -mfpu=fpv4-sp-d16
-    -DDeviceFamily_CC26X2)
+set(OBJ_GEN_FLAGS "-mcpu=cortex-m4 -march=armv7e-m -mfloat-abi=hard \
+    -mfpu=fpv4-sp-d16")
 
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OBJECT_GEN_FLAGS}"
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OBJ_GEN_FLAGS}"
     CACHE INTERNAL "C Compiler options")
-set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS} ${OBJECT_GEN_FLAGS}"
+set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS} ${OBJ_GEN_FLAGS}"
     CACHE INTERNAL "C++ Compiler options")
-set(CMAKE_ASM_FLAGS "${CMAKE_C_FLAGS} ${OBJECT_GEN_FLAGS}"
+set(CMAKE_ASM_FLAGS "${CMAKE_C_FLAGS} ${OBJ_GEN_FLAGS}"
     CACHE INTERNAL "ASM Compiler options")
